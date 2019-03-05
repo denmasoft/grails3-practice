12a0f9491e0716c02c47e413f8ea6080dbc2f02b
DenmaSoft <denmasoft@gmail.com>
2019-03-05 16:35:49 -0500

    v1.0

---
+++ grails-app/services/es/denmasoft/AuthorService.groovy
@@ -0,0 +1,19 @@
+package es.denmasoft
+
+
+class AuthorService {
+
+    /**
+     * Finds an author by its name
+     * @param name
+     * @return
+     */
+    List<Author> search(String name){
+        /* Other option is creating a criteria
+        Author.withCriteria{
+            like('name', "%${name}%")
+        }*/
+        //Dynamic finder - Best option for easier cases
+        Author.findAllByNameLike("%${name}%")
+    }
+}
